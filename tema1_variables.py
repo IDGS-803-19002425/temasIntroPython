6b41bb1da1bb7e57bb1e968a3f01d03eebcb560f
IDGS-803-19002425 <19002425@alumnos.utleon.edu.mx>
2025-01-13 22:20:39 -0600

    Commit ejercicios

---
+++ tema1_variables.py
@@ -0,0 +1,15 @@
+
+# int float string
+
+num1 = 10
+num2 = 20
+print(num1 + num2)
+num1= 'Pedro'
+
+print(num1)
+print(num2)
+print(num1)
+    
+numero3="23"
+numero4="12"
+print(int(numero3)+int(numero4))
---
+++ tema4_while.py
@@ -0,0 +1,9 @@
+
+print("Dame un numero ")
+x = int(input('Numero: '))
+cont = 0
+while cont < 10:    
+    cont = cont + 1
+    resultado= x * cont    
+    print(" {} * {} = {}".format(x,cont,resultado)) 
+    
